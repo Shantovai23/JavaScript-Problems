d552697086471036e796ec35fd9f3eb0f82c08e1
shantovai23 <hasibulhasan23sept@gmail.com>
2020-01-08 22:50:57 +0600

    JS

---
+++ Functions/Math functions.js
@@ -0,0 +1,13 @@
+//Math Functions
+
+
+var n= 7.34;
+
+ console.log(Math.floor(n));
+ console.log(Math.ceil(n));
+  console.log(Math.round(n));
+  console.log(Math.floor(Math.random()*10));
+  console.log(Math.max(100,30,200));
+  console.log(Math.min(3,1,230));
+  console.log(Math.pow(5,3));
+  console.log(Math.sqrt(9));

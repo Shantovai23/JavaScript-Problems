6832c1575634585f68a8499d8d29b77aa85ec41c
Hasibul Hasan Shanto <55630679+Shantovai23@users.noreply.github.com>
2020-01-08 23:05:41 +0600

    date functions

---
+++ Functions/date functions.js
@@ -0,0 +1,16 @@
+//date function
+
+
+ var date=new Date();
+ console.log(date);
+ console.log(date.toDateString())
+ console.log(date.toTimeString())
+ console.log(date.toLocaleString())
+
+
+ console.log(date.getFullYear())
+ console.log(date.getMonth()+1)  //Month start from 0 that's why we add 1 to get the correct month
+ console.log(date.getDate())
+ console.log(date.getHours())
+ console.log(date.getMinutes())
+ console.log(date.getSeconds())

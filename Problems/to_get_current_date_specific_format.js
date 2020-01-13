a6914c9edc7befb86a460f81efe53eaba34ccfe4
Hasibul Hasan Shanto <55630679+Shantovai23@users.noreply.github.com>
2020-01-13 18:24:28 +0600

    Js

---
+++ Problems/to_get_current_date_specific_format.js
@@ -0,0 +1,25 @@
+var date=new Date();
+var month=date.getMonth()+1;
+var day=date.getDate();
+var year=date.getFullYear();
+
+var today = month+'-'+day+'-'+year;
+console.log(today);
+    today = month+'/'+day+'/'+year;
+console.log(today);
+    today = day+'-'+month+'-'+year;
+console.log(today);
+   today = day+'/'+month+'/'+year;
+console.log(today);
+
+
+
+
+
+
+
+
+
+
+
+
